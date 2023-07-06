bcbc82ebfa2ffde2fd50f6228b8f7be4eff1cff9
ignam19 <ignaciomersch602811@gmail.com>
2023-07-05 22:49:53 -0300

    Probando funciones

--- index.py
+++ index.py
@@ -1,8 +1,30 @@
-def holamundo():
-    print("Hola mundo")
-    encabezado()
-    
-def encabezado():
-    print("Bienvenido")
-
+class Auto():
+    def __init__(self, puertas, color, modelo):
+        self.puertas = puertas
+        self.color = color
+        self.modelo = modelo
+        self.marcha = False 
+        
+    def arrancar(self):
+        if self.marcha == False:
+            self.marcha = True
+            
+            
+    def detener(self):
+        if self.marcha == True:
+            self.marcha == False
+            
+            
+    def estado(self):
+        if self.marcha == True:
+            print("El auto esta en marcha")
+        else:
+            print("El auto esta parado")    
+        
+auto1 = Auto(4, "Rojo", "Hilux")
+auto1.estado()
+            
+        
+        
+        
     
